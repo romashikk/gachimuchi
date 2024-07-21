4c8c0598a1231984d4cec296258de9e2e44c27ec
Romashikk <cemkapersunov410@gmail.com>
2024-07-21 11:11:26 +0500

    Update project configuration for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,29 +1,17 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Получение текущего каталога в ES-модулях
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
-app.use(express.json());
-
-// Настройка раздачи статических файлов
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Ваши API маршруты
-app.get('/api', (req, res) => {
-  res.send('Hello from the API!');
-});
-
-// Обработка всех остальных маршрутов и отправка index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
